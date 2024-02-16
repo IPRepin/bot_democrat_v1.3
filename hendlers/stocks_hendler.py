d4f5f6f23fd0b1f0a1afd64991532e511f346ee2
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-02-16 09:10:00 +0300

    Update stocks_hendler.py

--- hendlers/stocks_hendler.py
+++ hendlers/stocks_hendler.py
@@ -4,6 +4,7 @@ from aiogram import types, Router
 from aiogram.exceptions import TelegramBadRequest
 
 from data.sqlite_db_stocks import DatabaseStocks
+from keyboards.inline import not_entries_keyboard
 from keyboards.inline_kb_stocks import StocksInline
 
 router_stocks = Router()
@@ -22,7 +23,7 @@ async def show_description(callback_query: types.CallbackQuery,
     stock = db.select_stock(id=stock_id)
     logging.info(stock)
     try:
-        await callback_query.message.edit_text(stock[2])
+        await callback_query.message.edit_text(stock[2], reply_markup=not_entries_keyboard)
         await callback_query.answer()
     except TelegramBadRequest as e:
         logging.error(e)
