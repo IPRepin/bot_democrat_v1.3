5d43403c9b8dd10a7ee110b1b084d36edd414a0d
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-05-04 09:29:56 +0300

    Update connect_api_amo.py

--- amo_integration/connect_api_amo.py
+++ amo_integration/connect_api_amo.py
@@ -14,7 +14,7 @@ def connect_amo():
         client_secret=os.getenv('AMO_CLIENT_SECRET'),
         subdomain=os.getenv('AMO_SUBDOMAIN'),
         redirect_url=os.getenv('AMO_REDIRECT_URL'),
-        storage=tokens.FileTokensStorage(),  # by default FileTokensStorage
+        storage=tokens.FileTokensStorage(directory_path=os.getenv('AMO_STORAGE_DIR')),  # by default FileTokensStorage
     )
 
     # tokens.default_token_manager.init(code=os.getenv('AMO_TOKEN_MANAGER'),
