448a14ee38bd38b1399df12ef54c6f560c6ccc91
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-05-06 09:38:39 +0300

    add func edit stock

--- keyboards/inline_kb_stocks.py
+++ keyboards/inline_kb_stocks.py
@@ -10,6 +10,10 @@ class StocksInline(CallbackData, prefix="show_stocks"):
     action: str
 
 
+class EditStocksInline(CallbackData, prefix="edit_stocks"):
+    action: str
+
+
 async def choosing_promotion_keyboards():
     menu = InlineKeyboardBuilder()
     stocks = db.select_all_stocks()
@@ -17,6 +21,19 @@ async def choosing_promotion_keyboards():
         button_text = f"{stocks[i][1]}"
         id_stock = str(stocks[i][0])
         callback_data = StocksInline(action=id_stock).pack()
+        menu.row(
+            InlineKeyboardButton(text=button_text, callback_data=callback_data)
+        )
+    return menu.as_markup()
+
+
+async def edit_promotion_keyboards():
+    menu = InlineKeyboardBuilder()
+    stocks = db.select_all_stocks()
+    for i in range(len(stocks)):
+        button_text = f"{stocks[i][1]}"
+        id_stock = str(stocks[i][0])
+        callback_data = EditStocksInline(action=id_stock).pack()
         menu.row(
             InlineKeyboardButton(text=button_text, callback_data=callback_data)
         )
